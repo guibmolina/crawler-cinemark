f535389ebfd249adb6d6da9a47f49d0a9a1bea12
Guilherme Molina <guilhermemolina99@gmail.com>
2021-08-26 20:54:50 -0300

    feat: add json on header

--- src/RequestCinemark.php
+++ src/RequestCinemark.php
@@ -7,6 +7,8 @@ use Guibmolina\Cinemarkcrawler\Services\SearchMovies;
 
 require_once 'vendor/autoload.php';
 
+header('Content-type: application/json');
+
 $searchMovie = new SearchMovies(new Movie());
 
 echo $searchMovie->moviesOn('em-cartaz');
